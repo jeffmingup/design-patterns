85637e23efbfe20fddd0291a2500d90154f01906
ming <1960588251@qq.com>
2021-07-22 16:19:53 +0800

    strategy update

--- behavioral/strategy/strategy.go
+++ behavioral/strategy/strategy.go
@@ -6,50 +6,40 @@ import (
 )
 
 // 策略模式
-// 可以随意添加策略，而不用修改策略执行类的代码
+// 可以随意添加修改策略，而不用修改策略执行类的代码
 
-// 定义一个策略类
-type IStrategy interface {
-	do(int, int) int
-}
+type strategy func(int, int) int
 
-// 策略实现：加
-type add struct{}
-
-func (*add) do(a, b int) int {
+func strategyAdd(a, b int) int {
 	return a + b
 }
-
-// 策略实现：减
-type reduce struct{}
-
-func (*reduce) do(a, b int) int {
+func strategySub(a, b int) int {
 	return a - b
 }
 
 // 具体策略的执行者
 type Operator struct {
-	strategy IStrategy
+	strategy strategy
 }
 
 // 设置策略
-func (operator *Operator) setStrategy(strategy IStrategy) {
+func (operator *Operator) setStrategy(strategy strategy) {
 	operator.strategy = strategy
 }
 
 // 调用策略中的方法
 func (operator *Operator) calculate(a, b int) int {
-	return operator.strategy.do(a, b)
+	return operator.strategy(a, b)
 }
 
 func TestStrategy(t *testing.T) {
 	operator := Operator{}
 
-	operator.setStrategy(&add{})
+	operator.setStrategy(strategyAdd)
 	result := operator.calculate(1, 2)
 	fmt.Println("add:", result)
 
-	operator.setStrategy(&reduce{})
+	operator.setStrategy(strategySub)
 	result = operator.calculate(2, 1)
 	fmt.Println("reduce:", result)
 }
